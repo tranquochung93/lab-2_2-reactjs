99ca8546de398e9733aea8a1a5b8f0457e99f90b
tranquochung <quochung200693@gmail.com>
2022-01-04 09:41:59 +0700

    bailab2.2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react/cjs/react.production.min";
+import { Navbar,NavbarBrand } from "reactstrap";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component{
+render(){
+ return{
+  <div className="App">
+
+
+  <Navbar dark color="primary">
+    <div className="container">
+    
+    
+    
+    <NavbarBrand href="/">Ristorante CON FUSION </NavbarBrand>
+    
+    
+    
+    
+    
     </div>
-  );
+</Navbar>
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
-export default App;
+
+
+
+
+
+
+
+
+
+}
